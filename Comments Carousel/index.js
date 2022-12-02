cde4084cbcab0580c3403cebc1045e993e9040ba
ze-antunes <jprantunes2000@gmail.com>
2022-12-02 13:11:50 +0000

    comment carousel done

--- Comments Carousel/index.js
+++ Comments Carousel/index.js
@@ -1,47 +1,35 @@
 let file = './UFO_sights.csv'
 let carousel = document.querySelector(".pic-ctn");
+let i = 0
 
 const test = (start, end) => {
     const handleRequest = data => {
         console.log(data)
+
+        carousel.innerHTML = '';
+
         for (let i = 0; i < data.length; i++) {
             console.log(i, `<p class="pic" style=" animation-delay: ${i * 10}s">${data[i].comments}</p>`)
-            // let comment = document.createElement('p')
-            // comment.innerHTML = data[i].comments
-            // comment.classList.add('pic')
-            // carousel.append(comment)
             let str = `<p class="pic" style=" animation-delay: ${i * 10}s">${data[i].comments}</p>`
             carousel.insertAdjacentHTML('beforeend', str);
         }
-        setTimeout(() => {
-            // carousel.removeChild(carousel.lastChild);
-            console.log("ola")*10
-        }, 100000);
-
-        // setTimeout(function () {
-        //     setInterval(() => {
-        //         let elem = document.querySelector('.pic')
-        //         console.log(elem)
-        //         let state = window.getComputedStyle(elem)
-        //         console.log(state.animationPlayState)
-        //     }, 1000)
-        // }, 4000);
     }
 
     // Data Request 
     d3.csv(file, d3.autoType, (d, i) => {
-        // console.log(e, `, `, i)
-
         if (i < end && i >= start) return d
     }).then(handleRequest)
 }
 
-let i = 0
-
-function foo() {
-    console.log(i * 10, i * 10 + 10)
-    test(i * 10, i * 10 + 10)
-    i++
+const foo = () => {
+    if (i * 10 + 10 > 80330) {
+        i = 0;
+        test(i * 10, i * 10 + 10)
+        i++
+    } else {
+        test(i * 10, i * 10 + 10)
+        i++
+    }
     setTimeout(foo, 100000);
 }
 
